811a598fef9be78556dad84c99c060e100bb244e
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-31 17:54:10 -0800

    LoginForm -- update api.post call http

--- tieme-ndo/src/Components/LoginForm.js
+++ tieme-ndo/src/Components/LoginForm.js
@@ -47,7 +47,7 @@ export default function SignIn({ history }) {
     setIsLoading(true);
 
     axiosWithAuth()
-      .post(`http://localhost:5000/staff/login`, credentials)
+      .post(`https://tieme-ndo-5.herokuapp.com/staff/login`, credentials)
       .then(response => {
         console.log(response);
         localStorage.setItem("token", response.data.payload);
