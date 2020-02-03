39cca4a0eb7943113384994512d4c167af4801c5
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-03 13:51:37 -0800

    created actions/serverActions and reducer/index --> IP

--- tieme-ndo/src/Redux/actions/serverActions.js
+++ tieme-ndo/src/Redux/actions/serverActions.js
@@ -1,3 +1,22 @@
-import React from 'react';
+import axios from "axios";
+import { dispatch } from "rxjs/internal/observable/pairs";
 
-import { connect } from 'react'
+export const FETCH_CLIENTS_START = "FETCH_DOGS_START";
+export const FETCH_CLIENTS_SUCCESS = "FETCH_DOGS_SUCCESS";
+export const FETCH_CLIENTS_FAIL = "FETCH_DOGS_FAIL";
+
+// GET a list of Clients
+export const getClients = () => {
+  dispatch({ type: FETCH_CLIENTS_START });
+
+  axios
+    .get(``)
+    .then(response => {
+      console.log(response.data.message);
+      dispatch({ type: FETCH_CLIENTS_SUCCESS, payload: response.data.message });
+    })
+    .catch(error => {
+      console.log("Error =>", error);
+      dispatch({ type: FETCH_CLIENTS_FAIL, payload: error });
+    });
+};
