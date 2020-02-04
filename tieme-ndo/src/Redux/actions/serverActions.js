be5c794f01ae8224415cb8c2c625e1ffc2411eaa
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-04 13:53:39 -0800

    serverAction -- added Client requester Actions re: FETCH, POST, DELETE

--- tieme-ndo/src/Redux/actions/serverActions.js
+++ tieme-ndo/src/Redux/actions/serverActions.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 import { dispatch } from "rxjs/internal/observable/pairs";
 
-export const FETCH_CLIENTS_START = "FETCH_DOGS_START";
-export const FETCH_CLIENTS_SUCCESS = "FETCH_DOGS_SUCCESS";
-export const FETCH_CLIENTS_FAIL = "FETCH_DOGS_FAIL";
+//All Client Requester Actions
+export const FETCH_CLIENTS_START = "FETCH_CLIENTS_START";
+export const FETCH_CLIENTS_SUCCESS = "FETCH_CLIENTS_SUCCESS";
+export const FETCH_CLIENTS_FAIL = "FETCH_CLIENTS_FAIL";
+
+export const POST_CLIENT_DATA_START = "POST_CLIENT_DATA_START";
+export const POST_CLIENT_DATA_SUCCESS = "POST_CLIENT_DATA_SUCCESS";
+export const POST_CLIENT_DATA_FAILURE = "POST_CLIENT_DATA_FAILURE";
+
+export const DELETE_CLIENT_DATA_START = "DELETE_CLIENT_DATA_START";
+export const DELETE_CLIENT_DATA_SUCCESS = "DELETE_CLIENT_DATA_SUCCESS";
+export const DELETE_CLIENT_DATA_FAILURE = "DELETE_CLIENT_DATA_FAILURE";
+
+export const DELETE_CLIENT_ACCOUNT = "DELETE_CLIENT_ACCOUNT";
 
 // GET a list of Clients
 export const getClients = () => {
