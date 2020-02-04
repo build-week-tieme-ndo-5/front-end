3ebe2dd25eeadec82a676e54a4a6c0527ba16a5d
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-04 10:56:21 -0800

    Dashboard - fixed error on axiosWithAuth, no ; needed

--- tieme-ndo/src/Views/Dashboard.js
+++ tieme-ndo/src/Views/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { axiosWithAuth } from "../utilities/axiosWithAuth";
+import { axiosWithAuth } from "../Utilities/axiosWithAuth";
 
 import { Route, Redirect } from "react-router-dom";
 
 // import ClientCard from './'
 // import ClientForm from './'
 
-const Dashboard = () => {
+const Dashboard = props => {
   const [clientsList, setClientsList] = useState([]);
 
   //GET a list of Clients
@@ -24,12 +24,13 @@ const Dashboard = () => {
     getClientsList();
   }, []);
 
+  //POST/create a Client
   const addClient = clients => {
-      axiosWithAuth()
-        .post(`http://localhost:5000/clients/register`, clients);
-        .then(response => setClientsList(response.data)):
-        .catch(error => console.log("Error >", error.response));
-  }
+    axiosWithAuth()
+      .post("http://localhost:5000/clients/register", clients)
+      .then(response => setClientsList(response.data))
+      .catch(error => console.log("Error >", error.response));
+  };
 
   return (
     <div>
@@ -43,7 +44,7 @@ const Dashboard = () => {
         return <ClientCard key={client.id} client={client} />;
       })}
       <Route
-        path="/friends/edit/:id"
+        path="/clients/:id/update"
         render={props => {
           console.log(props);
           const currentClient = clientsList.find(
