7c78a83ce40705bd456bcfcddfd08b81fc367922
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-04 09:24:17 -0800

    index.js

--- tieme-ndo/src/index.js
+++ tieme-ndo/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import { Provider } from "react-dom";
+import { Provider } from "react-redux";
 import { createStore } from "redux";
 import clientReducer from "./Redux/reducers/clientReducer";
 
