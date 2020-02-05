f1696b063e4fc47c8699797657e99930de8d4b6b
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-04 17:49:09 -0800

    Michael -- fix to dashboard render

--- tieme-ndo/src/Views/ClientList.js
+++ tieme-ndo/src/Views/ClientList.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ClientList = () => {
+function ClientList(props) {
+  console.log(props);
+  const {client} = props
+
+  if(!props.client){
+    return (<h1>Loading...</h1>)
+  }
   return (
     <div>
-      <h4>ClientList</h4>
+      
+        <div key={client.id}>
+          <Link to={`/client-list/${client.id}`}>
+            <p>Client Name: {client.name}</p>
+          </Link>
+          <p>Village Name:{client.village}</p>
+          <p>Loan Amount: ${client.loan_amount}</p>
+        </div>
+      
     </div>
   );
-};
+}
+
+
 export default ClientList;
