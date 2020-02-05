ed7f0818e9dbf3e88a3d7d576777c0d73a0aabc1
Samuel Rowan <samuel@samuelrowan.com>
2020-02-04 20:26:28 -0600

    Added client list edit button

--- tieme-ndo/src/Views/ClientList.js
+++ tieme-ndo/src/Views/ClientList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from '@material-ui/core';
 
 function ClientList(props) {
   console.log(props);
@@ -18,7 +19,7 @@ function ClientList(props) {
           <p>Village Name:{client.village}</p>
           <p>Loan Amount: ${client.loan_amount}</p>
         </div>
-      
+        <Link to={`/client-list/${client.id}`}><Button variant="contained" color="primary">Edit</Button></Link>
     </div>
   );
 }
