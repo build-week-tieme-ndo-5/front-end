4affa087828429084880402101fdc149da7bc2e5
Michael Trew <mtrew2015@gmail.com>
2020-02-10 03:48:15 -0500

    built dashboard into table

--- tieme-ndo/src/Views/ClientList.js
+++ tieme-ndo/src/Views/ClientList.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React from 'react';
 import { axiosWithAuth } from '../Utilities/axiosWithAuth';
 import { useParams, useHistory } from 'react-router-dom';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
+import './ClientList.scss';
 
 function ClientList(props) {
-  const {client} = props
-  const { id }  = useParams();
-  const history = useHistory()
+	const { client } = props;
+	const { id } = useParams();
+	const history = useHistory();
 
-  if(!props.client){
-    return (<h1>Loading...</h1>)
-  }
-  console.log(typeof props)
+	if (!props.client) {
+		return <h1>Loading...</h1>;
+	}
+	console.log(typeof props);
 
-  const deleteClient = () => {
-    props.deleteFunction(client.id)
-  }
-  
-  return (
-    <div>
-      
-        <div key={client.id}>
-          <p>Client Name: {client.name}</p>
-          <p>Village Name:{client.village}</p>
-          <p>Loan Amount: ${client.loan_amount}</p>
-        </div>
-        <Link to={`/client-list/${client.id}`}><Button variant="contained" color="primary">Edit</Button></Link>
-        <Button onClick={deleteClient} variant="contained" color="secondary">Delete</Button>
-    </div>
-  );
+	const deleteClient = () => {
+		props.deleteFunction(client.id);
+	};
+
+	return (
+		<tr key={client.id} className='clients-container'>
+			<td> {client.name}</td>
+			<td>{client.village}</td>
+			<td> ${client.loan_amount}</td>
+			<td>
+				<Link to={`/client-list/${client.id}`}>
+					<Button variant='contained' color='primary'>
+						Edit
+					</Button>
+				</Link>
+			</td>
+			<td>
+				<Button onClick={deleteClient} variant='contained' color='secondary'>
+					Delete
+				</Button>
+			</td>
+		</tr>
+	);
 }
-
 
 export default ClientList;
--- tieme-ndo/src/Views/Dashboard.js
+++ tieme-ndo/src/Views/Dashboard.js
@@ -4,6 +4,7 @@ import { axiosWithAuth } from "../Utilities/axiosWithAuth";
 import { Link } from "react-router-dom";
 import { Button } from '@material-ui/core';
 import ClientList from "./ClientList";
+import './ClientList.scss'
 
 const Dashboard = props => {
   const {clientsList, setClientsList} = props;
@@ -18,13 +19,24 @@ const Dashboard = props => {
   }, []);
 
   return (
-    <div>
+    <div className="dashboard-wrapper">
+    <div className="dash-header">
       <h3>Dashboard</h3>
       <Link to={'/dashboard/client-add'}><Button variant="contained" color="primary">Add Client</Button></Link>
-      
+     </div> 
+    
+     <table>
+      <tr>
+        <th>Client Name</th>
+        <th>Village Name</th>
+        <th>Loan Amount</th>
+        <th>Edit Client</th>
+        <th>Delete</th>
+      </tr>
       {clientsList.map(client => {
         return <ClientList key={client.id} client={client} deleteFunction={deleteFunction} />;
       })}
+      </table>
     </div>
   );
 };
